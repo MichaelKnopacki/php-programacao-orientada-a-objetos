64e5dbe8cd680447fc09ed33f8459008ae2166f6
Michael Knopacki <michaelknopacki@hotmail.com>
2026-03-31 08:28:19 -0300

    20260331

--- php-programacao-orientada-a-objetos/screen-match/src/Modelo/Filme.php
+++ php-programacao-orientada-a-objetos/screen-match/src/Modelo/Filme.php
@@ -1,12 +1,37 @@
 <?php
 
 class Filme {
-    public string $nome;
-    public int $anoLancamento;
-    public string $genero;
-    public array $notas = [];
+    private string $nome;
+    private int $anoLancamento;
+    private string $genero;
+    private array $notas = [];
 
-    public function avalia($nota): void {
+    public function avalia(float $nota): void {
         $this->notas[] = $nota;
     }
+
+    public function nome(): string {
+        return $this->nome;
+    }
+
+    public function defineNome(string $nome ): void {
+        $this->nome = $nome;
+    }
+
+    public function ano():int {
+        return $this->anoLancamento;
+    }
+
+    public function defineAno(int $anoLancamento):void {
+        $this->anoLancamento = $anoLancamento;
+    }
+
+    public function genero():string{
+        return $this->genero;
+    }
+
+    public function defineGenero(string $genero):void{
+        $this->genero = $genero;
+    }
+
 }
